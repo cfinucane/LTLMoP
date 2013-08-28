ae113fc8b300d06aa9e84f7e39ed313cf006ee50
Cameron Finucane <cpf37@cornell.edu>
2013-08-28 15:53:49 -0400

    move analysis and core functions out of specCompiler; WIP, currently broken

---
+++ src/lib/analysis.py
@@ -0,0 +1,116 @@
+class SpecCompilerAnalysisExtensions(object):
+    def _autIsNonTrivial(self):
+        """
+        Check for a) empty automaton, or b) trivial initial-state automaton
+         with no transitions
+        (This can indicate unsatisfiable system initial conditions (case a),
+         or an unsat environment (case b).)
+
+        TODO: Do this in the Java code; it's super inefficient to
+        load the whole aut just to check this.
+        """
+
+        proj_copy = deepcopy(self.proj)
+        proj_copy.rfi = self.parser.proj.rfi
+        proj_copy.sensor_handler = None
+        proj_copy.actuator_handler = None
+        proj_copy.h_instance = None
+
+        aut = fsa.Automaton(proj_copy)
+
+        aut.loadFile(self.proj.getFilenamePrefix()+".aut", self.proj.enabled_sensors, self.proj.enabled_actuators, self.proj.all_customs)        
+        
+        nonTrivial = any([len(s.transitions) > 0 for s in aut.states])
+
+        return nonTrivial
+
+    def _analyze(self):
+        cmd = self._getGROneCommand("GROneDebug")
+        if cmd is None:
+            return (False, False, [], "")
+
+        subp = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, close_fds=False)
+
+        realizable = False    
+        unsat = False
+        nonTrivial = False
+        
+
+        output = ""
+        to_highlight = []
+        for dline in subp.stdout:
+            output += dline
+
+            if "Specification is realizable" in dline:   
+                realizable = True            
+                nonTrivial = self._autIsNonTrivial()
+                if nonTrivial:
+                    break
+            
+            ### Highlight sentences corresponding to identified errors ###
+
+            # System unsatisfiability
+            elif "System initial condition is unsatisfiable." in dline:
+                to_highlight.append(("sys", "init"))
+            elif "System transition relation is unsatisfiable." in dline:
+                to_highlight.append(("sys", "trans"))
+            elif "System highlighted goal(s) unsatisfiable" in dline:
+                for l in (dline.strip()).split()[-1:]:
+                    to_highlight.append(("sys", "goals", int(l)))
+            elif "System highlighted goal(s) inconsistent with transition relation" in dline:
+                to_highlight.append(("sys", "trans"))
+                to_highlight.append(("sys", "init"))
+                for l in (dline.strip()).split()[-1:]:
+                    to_highlight.append(("sys", "goals", int(l)))
+            elif "System initial condition inconsistent with transition relation" in dline:
+                to_highlight.append(("sys", "init"))
+                to_highlight.append(("sys", "trans"))
+           
+            # Environment unsatisfiability
+            elif "Environment initial condition is unsatisfiable." in dline:
+                to_highlight.append(("env", "init"))
+            elif "Environment transition relation is unsatisfiable." in dline:
+                to_highlight.append(("env", "trans"))
+            elif "Environment highlighted goal(s) unsatisfiable" in dline:
+                for l in (dline.strip()).split()[-1:]:
+                    to_highlight.append(("env", "goals", int(l)))
+            elif "Environment highlighted goal(s) inconsistent with transition relation" in dline:
+                to_highlight.append(("env", "init"))
+                to_highlight.append(("env", "trans"))
+                for l in (dline.strip()).split()[-1:]:
+                    to_highlight.append(("env", "goals", int(l)))
+            elif "Environment initial condition inconsistent with transition relation" in dline:
+                to_highlight.append(("env", "init"))
+                to_highlight.append(("env", "trans"))
+           
+        
+            # System unrealizability
+            elif "System is unrealizable because the environment can force a safety violation" in dline:
+                to_highlight.append(("sys", "trans"))
+                to_highlight.append(("sys", "init"))
+            elif "System highlighted goal(s) unrealizable" in dline:
+                to_highlight.append(("sys", "trans"))
+                to_highlight.append(("sys", "init"))
+                for l in (dline.strip()).split()[-1:]:
+                    to_highlight.append(("sys", "goals", int(l)))
+            
+            # Environment unrealizability
+            elif "Environment is unrealizable because the system can force a safety violation" in dline:
+                to_highlight.append(("env", "trans"))
+            elif "Environment highlighted goal(s) unrealizable" in dline:
+                to_highlight.append(("env", "trans"))
+                for l in (dline.strip()).split()[-1:]:
+                    to_highlight.append(("env", "goals", int(l)))
+                    
+            if "unsatisfiable" in dline or "inconsistent" in dline :
+                unsat = True
+
+        subp.stdout.close()
+        
+        
+        
+        return (realizable, unsat, nonTrivial, to_highlight, output)
+    
+       
+    
+    
